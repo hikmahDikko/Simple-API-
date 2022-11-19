b1832358102c2be3bd3dde1f067825365208dcf2
hikmahDikko <dhywis31@gmail.com>
2022-11-19 18:05:18 +0100

    read/write to file, slugify

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require("slugify");
+
 
 // /////////////////////////////////////////
 // //Files
@@ -47,6 +49,10 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
+
+const slugs = dataObject.map(el => slugify(el.productName, {lower:true}))
+
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true)
